e85eaf913e8ecf4844c48bc28663a4b6ad2088a6
Grant Forrest <grant.forrest@progress.com>
2025-08-12 17:41:08 -0400

    smaller

--- main.test.mjs
+++ main.test.mjs
@@ -1,5 +1,6 @@
 import {expect} from "vitest"
 import {it, mock, vi, beforeEach} from "vitest"
+import * as fs from "fs/promises"
 
 vi.mock("fs", () => ({
   readFileSync: () => `43
@@ -13,10 +14,15 @@ vi.mock("fs", () => ({
 let log = vi.spyOn(console, "log")
 
 it("produces the right output", async () => {
-  await import("./main.mjs")
+  await import("./src.mjs")
   expect(log).toHaveBeenNthCalledWith(1, "XLIII")
   expect(log).toHaveBeenNthCalledWith(2, "CIV")
   expect(log).toHaveBeenNthCalledWith(3, "CCXLIII")
   expect(log).toHaveBeenNthCalledWith(4, "CDI")
   expect(log).toHaveBeenNthCalledWith(5, "MMMCCLXXVII")
+})
+
+it("golfs", async () => {
+  const content = await fs.readFile("./main.mjs", "utf-8")
+  expect(content.length).toMatchInlineSnapshot(`320`)
 })
